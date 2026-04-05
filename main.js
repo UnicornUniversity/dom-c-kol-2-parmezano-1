51e6d16007daeb7fe5c35d600cc946ad02f5db25
Danylo Bykov <danilchick1612@gmail.com>
2026-04-05 17:43:38 +0200

    Added decimal to binary converter as domaci ukol 2

--- main.js
+++ main.js
@@ -1,35 +1,58 @@
-//TODO add imports if needed
-//import { exMain } from "./exclude/exampleAss2.js"
-//TODO add/change doc as needed
 /**
- * TODO - Write functional code for this application. You can call any other function, but usage of ".toString(numberSystem)" and "Number.parseInt(number, numberSystem)" is forbidden (only permitted when used on individual digits).
  * The main function which calls the application. 
- * TODO - Please, add specific description here for the application purpose.
+ * Converts a number from decimal to binary
  * @param {string} inputNumber number that is being converted
  * @param {number} inputNumberSystem numerical system that the inputNumber is being converted from
  * @param {number} outputNumberSystem numerical system that the inputNumber is being converted into
  * @returns {string} containing number converted to output system
  */
 export function main(inputNumber, inputNumberSystem, outputNumberSystem) {
-  //TODO code
+  let decimal = parseDecimalString(inputNumber)
+  let dtoOut = decimalToBinary(decimal)
   //let dtoOut = exMain(inputNumber, inputNumberSystem, outputNumberSystem);
   return dtoOut;
 }
 
 /**
- * TODO - Change this to contain all input number systems that your application can convert from.
+ * Parses a decimal string into a number 
+ * @param {string} input_string
+ * @returns {number}
+**/
+function parseDecimalString(input_string){
+  let converted_number = 0;
+  for (let digit of input_string){
+    converted_number = converted_number * 10 + Number.parseInt(digit);
+  }
+  return converted_number;
+}
+
+/**
+ * Converts a decimal number to a binary string
+ * @param {number} decimal_num 
+ * @returns {string}
+ */
+function decimalToBinary(decimal_num){
+  if (decimal_num === 0) return "0";
+  let binary_num = "";
+  while (decimal_num > 0) {
+    binary_num = (decimal_num % 2) + binary_num
+    decimal_num = (decimal_num - decimal_num % 2) / 2
+  }
+  return binary_num
+}
+
+/**
  * Function which returns which number systems are permitted on input.
  * @returns {Array} array of numbers refering to permitted input systems
  */
 export function permittedInputSystems() {
-	return [10, 2];
+	return [10];
 }
 
 /**
- * TODO - Change this to contain all output number systems that your application can convert to.
  * Function which returns which number systems are permitted on output.
  * @returns {Array} array of numbers refering to permitted output systems
  */
 export function permittedOutputSystems() {
-	return [10, 2];
+	return [2];
 }
